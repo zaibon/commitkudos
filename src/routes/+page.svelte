d89ef248945b88481f7491438421f2663ad3e4c7
cesarboucas <cesarboucas@gmail.com>
2023-11-29 18:15:43 +0000

    h2 class

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -167,7 +167,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 style="display: inline">
+		<h2 class="h2" style="display: inline">
 			<bold transition:slide>{greetings[index]}</bold> your top contributors
 		</h2>
 		<form class="w-full">
