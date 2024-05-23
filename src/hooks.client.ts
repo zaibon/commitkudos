c716b490467882af2f57ab59afede2bb430b3e7b
Christophe de Carvalho <christophe.dcpm@gmail.com>
2024-05-23 07:44:10 +0100

    fix sentry config

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,4 +1,4 @@
-import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
+import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 import { dev } from '$app/environment';
@@ -16,7 +16,7 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0,
 
 	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [new Replay()],
+	integrations: [Sentry.replayIntegration()],
 
 	environment: dev ? 'development' : 'production'
 });
